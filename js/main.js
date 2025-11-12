d8cbb7e528e16496915257f9207468478972e485
Agustina Pourrain <206278469+aguspourrain@users.noreply.github.com>
2025-11-12 17:01:45 -0300

    guia de compra y botones flotantes redes

--- js/main.js
+++ js/main.js
@@ -198,7 +198,7 @@ const carritoResumenEl = document.getElementById('carrito-resumen');
   // --- LÓGICA PARA ENVIAR PEDIDO A WHATSAPP ---
   btnEnviarPedido.addEventListener('click', () => {
 
-    alert("Estas a punto de hacer tu pedido! al continuar, vas a poder terminar de efectuar el pago y coordinar momento de entrega :) gracias ❤️");
+    //alert("Estas a punto de hacer tu pedido! al continuar, vas a poder terminar de efectuar el pago y coordinar momento de entrega :) gracias ❤️");
 
     let mensaje = '¡Hola Tinas Bake! Quisiera hacer el siguiente pedido:\n\n';
     
